e759fa6ef733be45caec9b792503c237baab4d63
Puzant <PBAKJEJIAN@hotmail.com>
2020-06-24 16:20:48 +0300

    add a new event to generate random values

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,12 +3,13 @@ import Counter from '../components/counter'
 
 class Counters extends Component {
   render() { 
-    const {onReset, onDecrement, onIncrement, onDelete, onSave, onClear, counters} = this.props
+    const {onReset, onDecrement, onIncrement, onDelete, onSave, onClear, onGenerateRandomValues, counters} = this.props
     return ( 
       <div>
         <button onClick={onReset} className="btn btn-primary btn-sm m-2">Reset All</button>
         <button onClick={onSave} className="btn btn-info btn-sm m-2">Save Counters</button>
         <button onClick={onClear} className="btn btn-danger btn-sm m-2">Clear Storage</button>
+        <button onClick={onGenerateRandomValues} className="btn btn-success btn-sm m-2">Generate Values</button>
         { counters.map(counter => (
           <Counter 
             onDelete={onDelete} 
