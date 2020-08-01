b9b0195c829a6135b5a1cce43ef166a4103da89a
Puzant <PBAKJEJIAN@hotmail.com>
2020-08-01 15:37:02 +0300

    add props type checks

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Counter extends Component {
 
-  render() { 
+  render() {
+    const { counterId } = this.props.counter.id
     return (
       <div className="counter-container">
-        <h3>Counter #{this.props.counter.id}</h3>
+        <h3>Counter #{counterId}</h3>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-primary btn-sm m-2">Increment</button>
         <button onClick={() => this.props.onDecrement(this.props.counter)} className="btn btn-secondary btn-sm">Decrement</button>
         <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+        <button onClick={() => this.props.onGenerateSingleValue(this.props.counter)} className="btn btn-success btn-sm m-2">Update</button>
       </div>
     );
   }
@@ -25,6 +28,14 @@ class Counter extends Component {
     return value === 0 ? 'Zero' : value
   }
 
+}
+
+Counter.propTypes = {
+ onDelete: PropTypes.func,
+ onIncrement: PropTypes.func,
+ onDecrement: PropTypes.func,
+ counter: PropTypes.object,
+ onGenerateSingleValue: PropTypes.func
 }
  
 export default Counter;
